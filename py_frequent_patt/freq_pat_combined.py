042b52b8d45fbdc809229db6ece36a33d0545d78
TangJiahui <tangjiahui0610@gmail.com>
2015-07-14 14:58:52 +0800

    change the input and output filename to command line

--- py_frequent_patt/freq_pat_combined.py
+++ py_frequent_patt/freq_pat_combined.py
@@ -9,8 +9,12 @@ def insertIntoDataStruct(name,location,mydict):
         mydict[name].extend([location])
 
 mydict={}
-with open("FPM_PFW3_Others.csv") as f:
-#with open("test.csv") as f:
+
+filename_i = raw_input('Please type in the filename here:')
+filename_o = filename_i[:-4]+'.basket'
+filename_r = filename_i[:-4]+'_result.csv'
+
+with open(filename_i) as f:
     reader = csv.reader(f)    
     for rows in reader:
         #rows = rows.split(',')
@@ -19,7 +23,7 @@ with open("FPM_PFW3_Others.csv") as f:
 transactions = list(mydict.values())
 
 ## write output into a format .basket that is suitable for Orange Package"
-with open("item.basket",'w',newline='') as f:
+with open(filename_o,'w') as f:
     writer = csv.writer(f,delimiter=',')
     for row in transactions:
         writer.writerow(row)
@@ -40,13 +44,14 @@ with open("item.basket",'w',newline='') as f:
 import Orange
 import csv
 
-items= Orange.data.Table("item.basket")
+items= Orange.data.Table(filename_o)
 rules= Orange.associate.AssociationRulesSparseInducer(items, support = 0.001, maxItemSets=1000000)
 rules=sorted(rules, key = lambda r: r.lift, reverse=True )
 
-with open("fre_result.csv","w") as f:
+with open(filename_r,"w") as f:
     writer1 = csv.writer(f,delimiter=' ')
     writer1.writerow(["supp","conf","lift","rule"])
     writer2 = csv.writer(f, delimiter=",")
     for r in rules:
         writer2.writerow(["%5.3f %5.3f %5.3f %s" % (r.support, r.confidence, r.lift, r)]) 
+
