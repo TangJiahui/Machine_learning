22ab2526a89b2ceb0cb1bcda166e1f687df5f6b3
TangJiahui <tangjiahui0610@gmail.com>
2015-07-15 13:52:30 +0800

    changed the read in arguments and output files

--- py_frequent_patt/combined_freq_pat.py
+++ py_frequent_patt/combined_freq_pat.py
@@ -1,6 +1,10 @@
+## run the script
+## >> python combined_freq_pat.py datafile.csv
+
 
 #transfer the dataset to another format
 import csv
+import sys
 
 def insertIntoDataStruct(name,location,mydict):
     if not name in mydict:
@@ -10,7 +14,7 @@ def insertIntoDataStruct(name,location,mydict):
 
 mydict={}
 
-filename_i = raw_input('Please type in the filename here:')
+filename_i = sys.argv[1]
 filename_o = filename_i[:-4]+'.basket'
 filename_r = filename_i[:-4]+'_result.csv'
 
@@ -80,20 +84,33 @@ thre_c = numpy.percentile(numpy.array(confidence),75) + 1.5*iqr_c
 rules_s=sorted(rules, key = lambda r: r.support, reverse=True )
 rules_c=sorted(rules, key = lambda r: r.confidence, reverse=True )
 
-filename_sign = filename_i[:-4]+'_significnat_csv'
-with open(filename_sign,"w") as f:
+
+filename_sign_l = filename_i[:-4]+'_significant_lift.csv'
+filename_sign_s = filename_i[:-4]+'_significant_support.csv'
+filename_sign_c = filename_i[:-4]+'_significant_confidence.csv'
+
+
+with open(filename_sign_l,"w") as f:
     writer1 = csv.writer(f, delimiter = ' ')
+    writer2 = csv.writer(f, delimiter=',')
     writer1.writerow(["Significant outliers derived by lift"])
     writer1.writerow(["supp","conf","lift","rule"])
-    writer2 = csv.writer(f, delimiter=',')
     for r in rules:
         if r.lift > thre_l:
             writer2.writerow(["%5.3f %5.3f %5.3f %s" % (r.support, r.confidence, r.lift, r)]) 
+
+with open(filename_sign_s,"w") as f:
+    writer1 = csv.writer(f, delimiter = ' ')
+    writer2 = csv.writer(f, delimiter=',')
     writer1.writerow(["Significant outliers derived by support"])
     writer1.writerow(["supp","conf","lift","rule"])
     for r in rules_s:
         if r.support > thre_s:
             writer2.writerow(["%5.3f %5.3f %5.3f %s" % (r.support, r.confidence, r.lift, r)]) 
+
+with open(filename_sign_c,"w") as f:
+    writer1 = csv.writer(f, delimiter = ' ')
+    writer2 = csv.writer(f, delimiter=',')
     writer1.writerow(["Significant outliers derived by confidence"])
     writer1.writerow(["supp","conf","lift","rule"])
     for r in rules_c:
